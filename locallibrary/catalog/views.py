7a2b3676ae02a7e6064d1aa2f52d564dfa6a5893
matthew-f-krueger <matthew.f.krueger@ndus.edu>
2024-03-25 17:10:55 -0500

    Completed Django Tutorial Part 5: Creating our home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Generate counts for genres and books containing a particular word
+    num_genres = Genre.objects.count()
+    word_count = 'The'  # Word to search for
+    num_books_with_word = Book.objects.filter(title__icontains=word_count).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'word_count': word_count,
+        'num_books_with_word': num_books_with_word,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
